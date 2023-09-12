f0aefa20454ed089b88943f248c9e424330324a7
loonpn <107356466+loonpn@users.noreply.github.com>
2023-09-12 10:36:29 +0800

    Update main.go
    Refactor the code.

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base32"
 	"fmt"
 	"io/ioutil"
 	"os"
-	"os/exec"
 	"os/user"
 	"strings"
 	"time"
@@ -15,116 +16,128 @@ import (
 )
 
 const (
-	
-	secretKey     = "IFBEGRCFIZDTCMRT"    // BASE32 encoding Key
-	timeout       = 30 * time.Second      // Timeout
+	length       = 6                  // Password length
+	timeout      = 30 * time.Second   // Timeout
+	algorithm    = otp.AlgorithmSHA1  // Encryption algorithm
 )
 
 var (
-	accountName     = ""
-	//path          = ""
-	//arguments     = ""
-	emergencyCodes []string
-	emergencyFile = "emergencycode"  // Emergency code save path
+	emergencyFile  string           // The path where the emergency code is saved
+	emergencyCodes []string         // Emergency code
+	secretKeyLen = 16               // Key length
 )
 
 func main() {
-	if isRunning() {
-		fmt.Println("The program is already running")
-		os.Exit(1)
-	}
 	currentUser, err := user.Current()
 	if err != nil {
-	   panic(err)
+		fmt.Println("Error: Unable to get user information")
 	}
-	name, err := os.Hostname()
+	hostname, err := os.Hostname()
 	if err != nil {
-		panic(err)
+		hostname = "unknowhost"
 	}
-	accountName = currentUser.Username + "@" + name
-	emergencyFile = currentUser.HomeDir + "/" + emergencyFile
+	home, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error: Unable to get user directory")
+		os.Exit(-3)
+	} else {
+		emergencyFile = home + "/.authenticator"
+	}
+	accountName := currentUser.Username + "@" + hostname
 
-	if isFirstRun() {
-		fmt.Println("This is your first time to run this program. Please scan the following QR code and save your key")
-		showQRCode()
-		fmt.Printf("Please enter verification code: ")
+	issuer := hostname
+	if len(os.Args) == 2 {
+		issuer = os.Args[1]
+	}
+	if isFirstRun(secretKeyLen) {
+		// Generate a key randomly
+		secretKey, err := randBase32String(length *2)
+		if err == nil {
+			secretKey = secretKey[:secretKeyLen]
+		}
+		url, err := showQRCode(accountName, issuer, secretKey)
+		if err != nil {
+			fmt.Println("Please generate QR code and scan:", url)
+		} else {
+			fmt.Println("Please scan the QR code using a password verifier app")
+		}
+		fmt.Printf("Please enter a random password: ")
 		code := readCode()
-		if verifyCode(code) {
-			fmt.Println("Your emergency scratch codes are:")
-			generateEmergencyCodes(6)
-			saveToFile()
+		if verifyCode(code, secretKey) {
+			err = generateEmergencyCodes(6) // Randomly generate 6 emergency codes
+			if err != nil {
+				fmt.Println("Error: Unable to generate emergency code")
+				os.Exit(-1)
+			}
+			err = saveToFile(secretKey)
+			if err != nil {
+				fmt.Println("Error: Unable to save file", emergencyFile)
+				os.Exit(-2)
+			}
+			fmt.Println("Validation successful, your key is:", secretKey, "\nPlease save the following emergency code:")
 			showEmergencyCodes()
 			os.Exit(0)
-		} else {
-			fmt.Println("Authenticate failed")
 		}
+		fmt.Println("Validation Failed")
 	} else {
-		readFromFile()
-		fmt.Printf("Please enter verification code: ")
+		secretKey, err := readFromFile(secretKeyLen)
+		if err != nil {
+			fmt.Println("Error: Cannot read file properly", emergencyFile)
+			os.Exit(-3)
+		}
+		fmt.Printf("Please enter a random password: ")
 		code := readCodeWithTimeout()
-		if verifyCode(code) {
-			fmt.Println("Authenticate successfully")
-			/*for k, v := range os.Args {
-				if k == 1 {
-					path = v
-				} else if k > 1 {
-					if arguments == "" {
-						arguments = v
-					} else {
-						arguments = arguments + " " + v
-					}
-				}
-			}*/
-			//runPathProgram()
+		if verifyCode(code, secretKey) {
 			os.Exit(0)
+		}
+		if code != "" {
+			fmt.Println("Validation Failed")
 		} else {
-			if code != "" {
-				fmt.Println("Authenticate failed")
-			}
+			fmt.Println()
 		}
 	}
 	os.Exit(1)
 }
 
-func isFirstRun() bool {
-	if _, err := os.Stat(emergencyFile); err == nil {
-		return false
+// Determine whether it is run for the first time
+func isFirstRun(secretKeyLen int) bool {
+	if f, err := os.Stat(emergencyFile); err == nil {
+		if f.IsDir() || f.Size() <= int64(secretKeyLen) || f.Size() > 1024 {
+			// The path is a directory, an empty file, or a file size that exceeds 1024 bytes
+			os.RemoveAll(emergencyFile)
+		} else {
+			return false
+		}
 	}
 	return true
 }
 
-func isRunning() bool {
-	cmd := exec.Command("pgrep", "-f", os.Args[0])
-	output, err := cmd.Output()
-	if err != nil {
-		return false
-	}
-	count := len(output) / 5 // Each process ID occupies 5 bytes
-	return count > 1         // If it is greater than 1, it indicates that the program is running
-}
-
-func showQRCode() {
-	key, err := totp.GenerateCodeCustom(secretKey, time.Now(), totp.ValidateOpts{
-		Digits:    6,
-		Algorithm: otp.AlgorithmSHA1,
+// Display the QR code character pattern
+func showQRCode(accountName, issuer, secretKey string) (string, error) {
+	url := fmt.Sprintf("otpauth://totp/%s?secret=%s&issuer=%s&algorithm=%s&digits=%d", accountName, secretKey, issuer, algorithm, length)
+	_, err := totp.GenerateCodeCustom(secretKey, time.Now(), totp.ValidateOpts{
+		Digits:    length,
+		Algorithm: algorithm,
 	})
 	if err != nil {
-		panic(err)
+		return url, err
 	}
-	url := fmt.Sprintf("otpauth://totp/%s?secret=%s&issuer=%s", accountName, secretKey, key)
 	qr, err := qrcode.New(url, qrcode.Medium)
 	if err != nil {
-		panic(err)
+		return url, err
 	}
 	fmt.Println(qr.ToSmallString(false))
+	return url, nil
 }
 
+// Read the password entered by the user
 func readCode() string {
 	var code string
 	fmt.Scanln(&code)
 	return code
 }
 
+// Read the password entered by the user with a timeout function
 func readCodeWithTimeout() string {
 	ch := make(chan string)
 	go func() {
@@ -140,44 +153,58 @@ func readCodeWithTimeout() string {
 	}
 }
 
-func verifyCode(code string) bool {
-	if len(code) != 6 && len(code) != 12 || !isDigit(code) {
+// Verify that the password entered by the user is correct
+func verifyCode(code, secretKey string) bool {
+	if !isDigitOrUpper(code) {
 		return false
 	}
-	for i, c := range emergencyCodes {
-		if code == c {
-			emergencyCodes[i] = ""
-			saveToFile()
-			return true
-		}
-	}
-	valid, err := totp.ValidateCustom(code, secretKey, time.Now(), totp.ValidateOpts{
-		Digits:    6,
-        Algorithm: otp.AlgorithmSHA1,
-	})
-	if err != nil {
-		return false
-	}
-	return valid
-}
-
-func generateEmergencyCodes(n int) {
-	emergencyCodes = make([]string, n)
-	for i := 0; i < n; i++ {
-		d := time.Duration(-i-1) * time.Minute
-		code, err := totp.GenerateCodeCustom(secretKey + secretKey, time.Now().Add(d), totp.ValidateOpts{
-			Digits:    12,
-			Algorithm: otp.AlgorithmSHA1,
+	if len(code) == length {
+		valid, err := totp.ValidateCustom(code, secretKey, time.Now(), totp.ValidateOpts{
+			Digits:    length,
+			Algorithm: algorithm,
 		})
 		if err != nil {
-			panic(err)
+			return false
 		}
-		emergencyCodes[i] = code
+		return valid
+	} else if len(code) == length * 2 {
+		for i, c := range emergencyCodes {
+			if code == c {
+				emergencyCodes[i] = ""
+				saveToFile(secretKey)
+				return true
+			}
+		}
 	}
+	return false
 }
 
-func saveToFile() {
-	str := ""
+// Randomly generate Base32 strings
+func randBase32String(n int) (string, error) {
+    b := make([]byte, n)
+    _, err := rand.Read(b)
+    if err != nil {
+        return "", err
+    }
+    return base32.StdEncoding.EncodeToString(b), nil
+}
+
+// Generate emergency codes randomly
+func generateEmergencyCodes(n int) error {
+	emergencyCodes = make([]string, n)
+	for i := n; i < 2 * n; i++ {
+		code, err := randBase32String(length * 2)
+		if err != nil {
+			return err
+		}
+		emergencyCodes[i - n] = code[:length *2]
+	}
+	return nil
+}
+
+// Save the emergency code to a file
+func saveToFile(secretKey string) error {
+	str := "# Your secret key is:\n" + secretKey + "\n# Your emergency codes are:\n"
 	for _, i := range emergencyCodes {
 		if i != "" {
 			str = str + i + "\n"
@@ -187,51 +214,73 @@ func saveToFile() {
 	f,err := os.Create(emergencyFile)
 	defer f.Close()
 	if err !=nil {
-		panic(err)
+		return err
 	}
 	_,err=f.Write([]byte(str))
 	if err != nil{
-		panic(err)
+		return err
 	}
+	return nil
 }
 
-func readFromFile() {
+// Read the file
+func readFromFile(secretKeyLen int) (string, error) {
 	content, err := ioutil.ReadFile(emergencyFile)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
+	secretKey := ""
 	lines := strings.Split(string(content), "\n")
-	for _, line := range lines {
-		if len(line) == 12 && isDigit(line) {
-			emergencyCodes = append(emergencyCodes, line)
+	for i, line := range lines {
+		linelen := len(line)
+		if linelen > 0 && int(line[0]) == 35 || linelen == 0 {
+			// The line is either a comment line or a blank line
+			continue
 		}
+		if i < 32 && isDigitOrUpper(line) {
+			// The line is a combination of numbers and uppercase letters
+			if secretKey == "" && linelen == secretKeyLen {
+				// The line is the key
+				secretKey = line
+			} else if secretKey != "" && linelen == length * 2 {
+				// This line is the emergency code and follows the key line
+				emergencyCodes = append(emergencyCodes, line)
+			} else {
+				// Illegal format: The key line is in the middle or after the emergency code line; Multiple keys
+				secretKey = ""
+				break
+			}
+		} else {
+			// Illegal format: The string is not a comment or a combination of numbers and uppercase letters; The number of rows exceeds the specified range
+			secretKey = ""
+			break
+		}
+	}
+	if secretKey != "" {
+		return secretKey, nil
+	} else {
+		return "", fmt.Errorf("Illegal format");
 	}
 }
 
-func isDigit(str string) bool {
+// Determines whether a string is a combination of numbers and uppercase letters
+func isDigitOrUpper(str string) bool {
+	// Iterate through each byte of the string
 	for i := 0; i < len(str); i++ {
+		// Gets the ASCII code corresponding to the byte
 		ascii := int(str[i])
-		if ascii < 48 || ascii > 57 {
+		// Determine if the ASCII code is not in the range of 0-9 and A-Z
+		if ascii < 48 || ascii > 57 && ascii < 65 || ascii > 90 {
+			// If not, return false
 			return false
 		}
 	}
 	return true
 }
 
+// Displays the emergency code
 func showEmergencyCodes() {
 	for _, c := range emergencyCodes {
 		fmt.Println(c)
 	}
 }
-/*
-func runPathProgram() {
-	if path == "" {
-		return
-	}
-	cmd := exec.Command(path, arguments)
-	err := cmd.Start()
-	if err != nil {
-		panic(err)
-	}
-}
-*/
